ed93a55044b980d33075d492dc6ac8038a201584
NikKowPHP <nik.kow@outlook.com>
2024-02-26 19:21:21 +0100

    feat: Initiate firebase app

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.PROJECT_ID,
+	messagingSenderId: process.env.PROJECT_ID,
+	appId: process.env.PROJECT_ID,
+});
