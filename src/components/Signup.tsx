5e511170251c7c3c4146eef6f3929a388dc7cb85
NikKowPHP <nik.kow@outlook.com>
2024-02-28 18:36:24 +0100

    create signup form

---
+++ src/components/Signup.tsx
@@ -0,0 +1,37 @@
+import { useRef } from "react";
+import { Button, Card, Form } from "react-bootstrap";
+export function Signup() {
+	const emailRef = useRef<HTMLInputElement | null>(null);
+	const passwordRef = useRef<HTMLInputElement | null>(null);
+	const passwordConfirmRef = useRef<HTMLInputElement | null>(null);
+	return (
+		<>
+			<Card>
+				<Card.Body>
+					<h2 className="text-center mb-4">Sign up</h2>
+					<Form>
+						<Form.Group id="email">
+							<Form.Label>Email</Form.Label>
+							<Form.Control type="email" ref={emailRef} required />
+						</Form.Group>
+						<Form.Group id="password">
+							<Form.Label>Email</Form.Label>
+							<Form.Control type="password" ref={passwordRef} required />
+						</Form.Group>
+						<Form.Group id="password-confirm">
+							<Form.Label>Password Confirmation</Form.Label>
+							<Form.Control type="email" ref={passwordConfirmRef} required />
+						</Form.Group>
+						<Button className="w-100" type="submit">
+							Sign Up
+						</Button>
+					</Form>
+				</Card.Body>
+				<div className="w-100 text-center mt-2"></div>
+			</Card>
+			<div className="w-100 text-center mt-2">
+				Already have an account? Log In
+			</div>
+		</>
+	);
+}
