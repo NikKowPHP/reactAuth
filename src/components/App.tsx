d089c79f2ff1a23344550da86295b2848edeb433
NikKowPHP <nik.kow@outlook.com>
2024-03-02 11:07:32 +0100

    feat: Add router to the app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,31 @@
 import { Container } from "react-bootstrap";
 import { Signup } from "./Signup";
-import { AuthProvider } from "../context/AuthContext";
+import { Login } from "./Login";
 
+import { AuthProvider } from "../context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Profile } from "../pages/Profile";
 
 function App() {
 	return (
-		<AuthProvider>
-			<Container
-				className="d-flex align-items-center justify-content-center"
-				style={{ minHeight: "100vh" }}
-			>
-				<div className="w-100" style={{ maxWidth: "400px" }}>
-					<Signup />
-				</div>
-			</Container>
-		</AuthProvider>
+		<Container
+			className="d-flex align-items-center justify-content-center"
+			style={{ minHeight: "100vh" }}
+		>
+			<div className="w-100" style={{ maxWidth: "400px" }}>
+				<Router>
+					<AuthProvider>
+						<Routes>
+							<Route path="/signup" element={<Signup/>}/>
+							<Route path="/login" element={<Login/>}/>
+							<Route path="/profile" element={<Profile/>}/>
+							<Route path="/" element={<Profile/>}/>
+							<Route></Route>
+						</Routes>
+					</AuthProvider>
+				</Router>
+			</div>
+		</Container>
 	);
 }
 
